5efaf4da1235bc9d855ce46b966c451c1c465978
AbdulManafAbdullah <mannanzuni91@gmail.com>
2024-07-13 21:43:24 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+// Day 1: Variables and Data Types
+
+// Tasks/Activities:
+// Activity 1: Variable Declaration
+// • Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+
+// var chai = 78;
+// console.log(chai);
+
+// • Task 2: Declare a variable using let, assign it a string, and log the value to the console.
+
+// let code = "with hitesh";
+// console.log(code);
+
+// Activity 2: Constant Declaration
+// ⚫ Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+
+// const bool = false;
+// console.log(bool);
+
+// Activity 3: Data Types
+// • Task 4: Create variables of different data types (number, string, boolean, object, array)
+//  and log each variable's type using the type of operator.
+
+// Number
+// const num = 38;
+// console.log(typeof num);
+
+// String
+// const str = "Saylani";
+// console.log(typeof str);
+
+// Boolean
+// const coding = true;
+// console.log(typeof coding);
+
+// Object
+// const obj = {
+//   name: "abdulMunaf",
+//   price: 0,
+//   topic: "programming",
+// };
+// console.log(typeof obj);
+
+//Array
+// const arr = [32, 39, 30];
+// console.log(typeof arr);
+
+// Activity 4: Reassigning Variables.
+// Task 5: Declare a variable using let, assign it an initial value, reassign a new value, 
+// and log both values to the console.
+
+// let val = "saylaniMass";
+// console.log("initial value ==>",  val);
+
+// val = "chai or code";
+// console.log("reassign value ==>", val);
+
+// Activity 5: Understanding const
+// Task 6: Try reassigning a variable declared with const and observe the error.
+
+// const value = "First time giving test honestly";
+// console.log("initial value ==>",val);
+
+// // const value <====SyntaxError=====> Error
+
+// const value = "and learning well";
+// console.log("reassign value ==>", value); Error 
+// Note: Cannot redeclare block-scoped variable...
+
+
+// Achievement:
+// By the end of these activities, you will:
+// • Know how to declare variables using var, let, and const.
+// • Understand the different data types in JavaScript.
+// • Be able to use the typeof operator to identify the data type of a variable.
+// • Understand the concept of variable reassignment and the immutability of const variables.
